6cc5ee3ea9226db9dbcbe036786744512c349301
Christian Chunga <christian98.cb@outlook.es>
2024-10-15 23:36:49 -0500

    Navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import {Navbar} from "@/components/system-design/navbar";
 import {GitHubLogoIcon, LinkedInLogoIcon} from "@radix-ui/react-icons";
 import Image from "next/image";
 
@@ -6,7 +5,6 @@ export default function Home() {
     return (
         <div
             className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-            <Navbar/>
             <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
                 <Image
                     className="dark:invert"
--- src/components/system-design/navbar.tsx
+++ src/components/system-design/navbar.tsx
@@ -15,87 +15,52 @@ import {PersonIcon} from "@radix-ui/react-icons";
 import Link from "next/link"
 import * as React from "react"
 
-const components: { title: string; href: string; description: string }[] = [
-    {
-        title: "Alert Dialog",
-        href: "/docs/primitives/alert-dialog",
-        description:
-            "A modal dialog that interrupts the user with important content and expects a response.",
-    },
-    {
-        title: "Hover Card",
-        href: "/docs/primitives/hover-card",
-        description:
-            "For sighted users to preview content available behind a link.",
-    },
-    {
-        title: "Progress",
-        href: "/docs/primitives/progress",
-        description:
-            "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
-    },
-    {
-        title: "Scroll-area",
-        href: "/docs/primitives/scroll-area",
-        description: "Visually or semantically separates content.",
-    },
-    {
-        title: "Tabs",
-        href: "/docs/primitives/tabs",
-        description:
-            "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
-    },
-    {
-        title: "Tooltip",
-        href: "/docs/primitives/tooltip",
-        description:
-            "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
-    },
-]
-
 export function Navbar() {
     return (
-        <NavigationMenu>
-            <NavigationMenuList>
-                <NavigationMenuItem>
-                    <Link href="/" legacyBehavior passHref>
-                        <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                            Home
-                        </NavigationMenuLink>
-                    </Link>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                    <NavigationMenuTrigger>Data</NavigationMenuTrigger>
-                    <NavigationMenuContent>
-                        <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-                            <li className="row-span-3">
-                                <NavigationMenuLink asChild>
-                                    <a
-                                        className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-                                        href="/users"
-                                    >
-                                        <PersonIcon className="h-6 w-6"/>
-                                        <div className="mb-2 mt-4 text-lg font-medium">
-                                            Users
-                                        </div>
-                                        <p className="text-sm leading-tight text-muted-foreground">
-                                            Main form, registers users in the system. This is the core of the system and
-                                            contains all the requirements listed in the test.
-                                        </p>
-                                    </a>
-                                </NavigationMenuLink>
-                            </li>
-                            <ListItem href="/family" title="Family">
-                                Contains summarized data on users&apos; family members.
-                            </ListItem>
-                            <ListItem href="/crops" title="Crops">
-                                Contains summary data on user&apos;s crops.
-                            </ListItem>
-                        </ul>
-                    </NavigationMenuContent>
-                </NavigationMenuItem>
-            </NavigationMenuList>
-        </NavigationMenu>
+        <div className="w-full flex justify-center items-center font-[family-name:var(--font-geist-sans)] my-6">
+            <NavigationMenu>
+                <NavigationMenuList>
+                    <NavigationMenuItem>
+                        <Link href="/" legacyBehavior passHref>
+                            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                                Home
+                            </NavigationMenuLink>
+                        </Link>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                        <NavigationMenuTrigger>Data</NavigationMenuTrigger>
+                        <NavigationMenuContent>
+                            <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+                                <li className="row-span-3">
+                                    <NavigationMenuLink asChild>
+                                        <a
+                                            className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                                            href="/users"
+                                        >
+                                            <PersonIcon className="h-6 w-6"/>
+                                            <div className="mb-2 mt-4 text-lg font-medium">
+                                                Users
+                                            </div>
+                                            <p className="text-sm leading-tight text-muted-foreground">
+                                                Main form, registers users in the system. This is the core of the system
+                                                and
+                                                contains all the requirements listed in the test.
+                                            </p>
+                                        </a>
+                                    </NavigationMenuLink>
+                                </li>
+                                <ListItem href="/family" title="Family">
+                                    Contains summarized data on users&apos; family members.
+                                </ListItem>
+                                <ListItem href="/crops" title="Crops">
+                                    Contains summary data on user&apos;s crops.
+                                </ListItem>
+                            </ul>
+                        </NavigationMenuContent>
+                    </NavigationMenuItem>
+                </NavigationMenuList>
+            </NavigationMenu>
+        </div>
     )
 }
 
